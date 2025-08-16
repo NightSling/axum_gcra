539688f9b536800ab79b520eae14e9ea9f5e6e96
Sushant Pangeni <daysling@daysling.com>
2025-08-16 11:08:07 +0545

    change real_ip implementation

--- src/real_ip.rs
+++ src/real_ip.rs
@@ -1,16 +1,9 @@
-//! Provides a way to get the real IP address of the client, to the best of our ability.
-//!
-//! This is exposed as [`RealIp`], which is an extractor that can be used in Axum routes,
-//! or as a [`Layer`] and [`Service`] that can be used to add the IP address to
-//! the request parts extensions. Or both.
-
 use std::{
     fmt::{self, Debug, Display},
-    future::Future,
+    future,
     hash::Hash,
     net::{IpAddr, SocketAddr},
     ops::Deref,
-    pin::Pin,
     str::FromStr,
     task::{Context, Poll},
 };
@@ -21,38 +14,13 @@ use tower::{Layer, Service};
 
 /// Wrapper around [`std::net::IpAddr`] that can be extracted from the request parts.
 ///
-/// This extractor will try to get the real IP address of the client, using the following headers, in order:
-/// - `cf-connecting-ip` (used by Cloudflare sometimes)
-/// - `x-cluster-client-ip` (used by AWS sometimes)
-/// - `fly-client-ip` (used by Fly.io sometimes)
-/// - `fastly-client-ip` (used by Fastly sometimes)
-/// - `cloudfront-viewer-address" (used by Cloudfront sometimes)
-/// - `x-real-ip`
-/// - `x-forwarded-for`
-/// - `x-original-forwarded-for` (maybe used by Cloudfront?)
-/// - `true-client-ip` (used by some load balancers)
-/// - `client-ip` (used by some load balancers)
-///
-/// If none of these headers are found, it will return a 400 Bad Request via [`IpAddrRejection`],
-/// or the error can be handled with a custom rejection handler with
-/// [`RateLimitLayerBuilder::handle_error`](crate::RateLimitLayerBuilder::handle_error).
-///
-/// If the `tokio` feature is enabled AND the `Router` is configured with
-/// [`Router::into_make_service_with_connect_info`](axum::Router::into_make_service_with_connect_info),
-/// it will also try to get the IP address from the underlying socket via the [`ConnectInfo<SocketAddr>`](axum::extract::ConnectInfo) extension.
-/// This is optional as it may not work as expected if the server is behind a reverse proxy.
-///
-/// The [`RealIpLayer`] can be also used to add the [`RealIp`] extension to the request if available, allowing
-/// other services or extractors to reuse it without rescanning the headers every time.
+/// This extractor tries to resolve the client's IP address from common proxy/load balancer headers,
+/// falling back to the underlying socket (if `ConnectInfo` is enabled).
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[repr(transparent)]
 pub struct RealIp(pub IpAddr);
 
-/// Like [`RealIp`], but with the last 64 bits of any IPv6 address set to zeroes.
-///
-/// This is useful for making sure clients with randomized IPv6 interfaces
-/// aren't treated as different clients. This can be common in some networks
-/// that attempt to preserve privacy.
+/// Like [`RealIp`], but with the last 64 bits of IPv6 addresses zeroed out.
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[repr(transparent)]
 pub struct RealIpPrivacyMask(pub RealIp);
@@ -60,38 +28,37 @@ pub struct RealIpPrivacyMask(pub RealIp);
 impl From<RealIp> for RealIpPrivacyMask {
     #[inline]
     fn from(ip: RealIp) -> Self {
-        RealIpPrivacyMask(match ip.0 {
-            IpAddr::V4(ip) => RealIp(IpAddr::V4(ip)),
-            IpAddr::V6(ip) => RealIp(IpAddr::V6(From::from(
-                ip.to_bits() & 0xFFFF_FFFF_FFFF_FFFF_0000_0000_0000_0000,
-            ))),
-        })
+        match ip.0 {
+            IpAddr::V4(v4) => RealIpPrivacyMask(RealIp(IpAddr::V4(v4))),
+            IpAddr::V6(v6) => {
+                let mut segments = v6.segments();
+                // zero out lower 4 segments = last 64 bits
+                segments[4] = 0;
+                segments[5] = 0;
+                segments[6] = 0;
+                segments[7] = 0;
+                RealIpPrivacyMask(RealIp(IpAddr::V6(std::net::Ipv6Addr::from(segments))))
+            }
+        }
     }
 }
 
 impl Debug for RealIp {
-    #[inline]
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         Debug::fmt(&self.0, f)
     }
 }
-
 impl Display for RealIp {
-    #[inline]
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         Display::fmt(&self.0, f)
     }
 }
-
 impl Debug for RealIpPrivacyMask {
-    #[inline]
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         Debug::fmt(&self.0, f)
     }
 }
-
 impl Display for RealIpPrivacyMask {
-    #[inline]
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         Display::fmt(&self.0, f)
     }
@@ -99,23 +66,18 @@ impl Display for RealIpPrivacyMask {
 
 impl Deref for RealIp {
     type Target = IpAddr;
-
-    #[inline]
     fn deref(&self) -> &Self::Target {
         &self.0
     }
 }
-
 impl Deref for RealIpPrivacyMask {
     type Target = RealIp;
-
-    #[inline]
     fn deref(&self) -> &Self::Target {
         &self.0
     }
 }
 
-/// IP Address not found, returns a 400 Bad Request.
+/// IP Address not found, returns 400.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct IpAddrRejection;
 
@@ -125,58 +87,39 @@ impl IntoResponse for IpAddrRejection {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
-#[repr(transparent)]
-struct IpFut<T>(Result<T, IpAddrRejection>);
-
-impl<T: Copy> Future for IpFut<T> {
-    type Output = Result<T, IpAddrRejection>;
-
-    #[inline]
-    fn poll(self: Pin<&mut Self>, _: &mut Context<'_>) -> Poll<Self::Output> {
-        Poll::Ready(self.0)
-    }
-}
-
 impl<S> FromRequestParts<S> for RealIp {
     type Rejection = IpAddrRejection;
 
-    fn from_request_parts(parts: &mut Parts, _: &S) -> impl Future<Output = Result<Self, Self::Rejection>> + Send {
+    fn from_request_parts(
+        parts: &mut Parts,
+        _: &S,
+    ) -> impl std::future::Future<Output = Result<Self, Self::Rejection>> + Send {
         if let Some(ip) = parts.extensions.get::<RealIp>() {
-            return IpFut(Ok(*ip));
+            return future::ready(Ok(*ip));
         }
-
-        IpFut(match get_ip_from_parts(parts) {
-            Some(ip) => Ok(ip),
-            None => Err(IpAddrRejection),
-        })
+        future::ready(get_ip_from_parts(parts).ok_or(IpAddrRejection))
     }
 }
 
 impl<S> FromRequestParts<S> for RealIpPrivacyMask {
     type Rejection = IpAddrRejection;
 
-    fn from_request_parts(parts: &mut Parts, _: &S) -> impl Future<Output = Result<Self, Self::Rejection>> + Send {
-        if let Some(&ip) = parts.extensions.get::<RealIp>() {
-            return IpFut(Ok(ip.into()));
+    fn from_request_parts(
+        parts: &mut Parts,
+        _: &S,
+    ) -> impl std::future::Future<Output = Result<Self, Self::Rejection>> + Send {
+        if let Some(ip) = parts.extensions.get::<RealIp>() {
+            return future::ready(Ok((*ip).into()));
         }
-
-        IpFut(match get_ip_from_parts(parts) {
-            Some(ip) => Ok(ip.into()),
-            None => Err(IpAddrRejection),
-        })
+        future::ready(get_ip_from_parts(parts).map(Into::into).ok_or(IpAddrRejection))
     }
 }
 
-/// [`Service`] that adds the [`RealIp`] extension to the request parts if available.
-///
-/// This extension can be reused by other services or extractors, such as [`RealIp`] itself.
+/// Service that adds the [`RealIp`] extension.
 #[derive(Debug, Clone, Copy)]
 pub struct RealIpService<I>(I);
 
-/// [`Layer`] that adds the [`RealIp`] extension to the request parts if available.
-///
-/// This extension can be reused by other services or extractors, such as [`RealIp`] itself.
+/// Layer that adds the [`RealIp`] extension.
 #[derive(Debug, Clone, Copy)]
 pub struct RealIpLayer;
 
@@ -212,29 +155,39 @@ impl<I> Layer<I> for RealIpLayer {
 }
 
 pub(crate) fn get_ip_from_parts(parts: &Parts) -> Option<RealIp> {
-    fn parse_ip(s: &HeaderValue) -> Option<IpAddr> {
-        s.to_str()
-            .ok()
-            .and_then(|s| s.split(&[',', ':']).next())
-            .and_then(|s| IpAddr::from_str(s.trim()).ok())
+    fn parse_ip(val: &HeaderValue, allow_port: bool) -> Option<IpAddr> {
+        let s = val.to_str().ok()?.trim();
+
+        // Split on `,` for multi-hop headers (take the first entry)
+        let first = s.split(',').next()?.trim();
+
+        if allow_port {
+            // Handle `IP:port` (CloudFront, some proxies)
+            if let Ok(sock) = SocketAddr::from_str(first) {
+                return Some(sock.ip());
+            }
+        }
+        IpAddr::from_str(first).ok()
     }
 
-    static HEADERS: [HeaderName; 10] = [
-        HeaderName::from_static("cf-connecting-ip"), // used by Cloudflare sometimes
-        HeaderName::from_static("x-cluster-client-ip"), // used by AWS sometimes
-        HeaderName::from_static("fly-client-ip"),    // used by Fly.io sometimes
-        HeaderName::from_static("fastly-client-ip"), // used by Fastly sometimes
-        HeaderName::from_static("cloudfront-viewer-address"), // used by Cloudfront sometimes
-        HeaderName::from_static("x-real-ip"),
-        HeaderName::from_static("x-forwarded-for"),
-        HeaderName::from_static("x-original-forwarded-for"), // maybe used by Cloudfront?
-        HeaderName::from_static("true-client-ip"),           // used by some load balancers
-        HeaderName::from_static("client-ip"),                // used by some load balancers
+    static HEADERS: [(HeaderName, bool); 10] = [
+        (HeaderName::from_static("cf-connecting-ip"), false),
+        (HeaderName::from_static("x-cluster-client-ip"), false),
+        (HeaderName::from_static("fly-client-ip"), false),
+        (HeaderName::from_static("fastly-client-ip"), false),
+        (HeaderName::from_static("cloudfront-viewer-address"), true), // IP:port
+        (HeaderName::from_static("x-real-ip"), false),
+        (HeaderName::from_static("x-forwarded-for"), false), // may contain list
+        (HeaderName::from_static("x-original-forwarded-for"), false),
+        (HeaderName::from_static("true-client-ip"), false),
+        (HeaderName::from_static("client-ip"), false),
     ];
 
-    for header in &HEADERS {
-        if let Some(real_ip) = parts.headers.get(header).and_then(parse_ip) {
-            return Some(RealIp(real_ip));
+    for (header, allow_port) in &HEADERS {
+        if let Some(val) = parts.headers.get(header) {
+            if let Some(ip) = parse_ip(val, *allow_port) {
+                return Some(RealIp(ip));
+            }
         }
     }
 
